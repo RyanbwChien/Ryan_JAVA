715eb238fb2a4ff65b8c7f25a8a09b2ca2e64ccf
DESKTOP-OQJ6D6B\Ryan <saikidd2002@gmail.com>
2025-07-28 02:01:11 +0800

    no message

---
+++ Myclass.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class Myclass{
+    static int x=100;
+    public static void main(String[] args){
+        System.out.println("AAA");
+        System.out.println(Myclass.x);
+    }
+
+}
