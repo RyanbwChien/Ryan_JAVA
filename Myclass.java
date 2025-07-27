35a7e3f594a4f51fa7e315dbbc002040099416fb
DESKTOP-OQJ6D6B\Ryan <saikidd2002@gmail.com>
2025-07-28 02:42:00 +0800

    no message

--- Myclass.java
+++ Myclass.java
@@ -5,7 +5,11 @@
 public class Myclass{
     static int x=100;
     public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int cost = a +b;
         System.out.println("AAA");
+        System.out.println(cost);
         System.out.println(Myclass.x);
     }
 
