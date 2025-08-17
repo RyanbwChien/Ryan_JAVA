1309c0ed89c6a1be5d48bacb852ad2cc962f631f
MSI\USER <saikidd2002@yahoo.com.tw>
2025-08-18 04:12:46 +0800

    no message

--- Myclass.java
+++ Myclass.java
@@ -1,9 +1,13 @@
 
 
-
-
 public class Myclass{
     static int x=100;
+
+    public static class news2{
+    int x = 3000;
+
+
+    }
     public static void main(String[] args){
         int a = 10;
         int b = 20;
@@ -11,6 +15,8 @@ public class Myclass{
         System.out.println("AAA");
         System.out.println(cost);
         System.out.println(Myclass.x);
+        news2 nn = new news2();
+        System.out.println(nn.x);
     }
 
 }
